6a5a4225243dd90ef30d9fbc4b6ed06b3a60a2f5
Felipe Tavares <felipe.oltavares@gmail.com>
2015-09-23 02:35:20 -0300

    Finished most of the editor.
    Added export fuctions.

--- editor/js/color.js
+++ editor/js/color.js
@@ -13,7 +13,7 @@ function ColorNode (color, data) {
     }
 
     this.del = function (s, e, poetry) {
-	console.log ("del("+s+","+e+") from "+this.data);
+	//console.log ("del("+s+","+e+") from "+this.data);
 	
 	this.data = this.data.substr(0, s)+this.data.substr(e, this.data.length-e);
 
@@ -30,8 +30,47 @@ function ColorNode (color, data) {
 	}
     }
 
+    this.change = function (s, e, poetry, ncolor) {
+	//console.log ("change("+s+","+e+","+ncolor+") from "+this.data);
+	
+	if (s == 0 && e == this.data.length) {
+	    this.color = ncolor;
+	    return;
+	}
+
+	if (s > 0) {
+	    var newMe = new ColorNode(this.color, this.data.substr(0, s));
+	    
+	    newMe.prev = this.prev;
+	    newMe.next = this;
+	    
+	    if (this.prev) {
+		this.prev.next = newMe;
+	    } else
+		poetry.updateTail(this, newMe);
+
+	    this.prev = newMe;
+	}
+
+	if (e < this.data.length) {
+	    var newMe = new ColorNode(this.color, this.data.substr(e, this.data.length-e));
+	    newMe.prev = this;
+	    newMe.next = this.next;
+
+	    if (this.next) {
+		this.next.prev = newMe;
+	    } else
+		poetry.updateHead(this, newMe);
+	    
+	    this.next = newMe;
+	}
+
+	this.data = this.data.substr(s, e-s);
+	this.color = ncolor;
+    }
+
     this.insert = function (p, w, poetry) {
-	console.log ("insert at "+p+" of "+w+" in block "+this.data);
+	//console.log ("insert at "+p+" of "+w+" in block "+this.data);
 
 	if (poetry.color == this.color) {
 	    this.data = this.data.splice (p, 0, w);
@@ -115,6 +154,25 @@ function CompiledPoetry () {
 
     this.setColor = function (c) {
 	this.color = c;
+	var selections = editor.listSelections();
+
+	if (selections.length > 0) {
+	    for (var s in selections) {
+		var start = toLinear(selections[s].head);
+		var end = toLinear(selections[s].anchor);
+
+		if (start > end) {
+		    var buf = start;
+		    start = end;
+		    end = buf;
+		}
+
+		if (start-end != 0)
+		    this.changeAt(start, end, this.color);
+	    }
+	}
+
+	editor.focus();
     }
 
     // A detailded description of the algorithm used can be found
@@ -139,7 +197,7 @@ function CompiledPoetry () {
 	    i = n;
 	    n = n.next;
 
-	    console.log (sp, nsp);
+	    //console.log (sp, nsp);
 
 	    if (sp <= p && !(nsp <= p))
 		break;
@@ -148,9 +206,13 @@ function CompiledPoetry () {
 	i.insert (p-sp, w, this);
     }
 
-    this.popAt = function (s, e) {
+    this.changeAt = function (s, e, c) {
 	var n = this.tail;
 	var i = n;
+
+	if (!n)
+	    return;
+
 	// Block start position, next block start position
 	var sp = 0, nsp = 0;
 	do {
@@ -159,7 +221,49 @@ function CompiledPoetry () {
 	    i = n;
 	    n = n.next;
 
-	    console.log (sp, nsp);
+	    //console.log (sp, nsp);
+
+	    if (sp <= s && !(nsp <= s))
+		break;
+	} while (n);
+
+	i.change(s-sp, this.ending(sp, nsp, e), this, c);
+
+	do {
+	    if (n) {
+		sp = nsp;
+		nsp += n.lenght();
+		i = n;
+		n = n.next;
+		
+		if (i) {
+		    if (sp <= e)
+			i.change(0, this.ending(sp, nsp, e), this, c);
+		    else
+			break;
+		}
+	    }
+	} while (n);
+
+	this.renderOnCanvas();
+    }
+
+    this.popAt = function (s, e) {
+	var n = this.tail;
+	var i = n;
+	// Block start position, next block start position
+	var sp = 0, nsp = 0;
+
+	if (!n)
+	    return;
+
+	do {
+	    sp = nsp;
+	    nsp += n.lenght();
+	    i = n;
+	    n = n.next;
+
+	    //console.log (sp, nsp);
 
 	    if (sp <= s && !(nsp <= s))
 		break;
@@ -202,6 +306,22 @@ function CompiledPoetry () {
 	}
     }
 
+    this.merge = function () {
+	var n = this.tail;
+	while (n) {
+	    if (n.next) {
+		if (n.color == n.next.color) {
+		    n.data += n.next.data;
+		    if (n.next.next) {
+			n.next.next.prev = n;
+		    }
+		    n.next = n.next.next;
+		}
+	    }
+	    n = n.next;
+	}
+    }
+
     this.render = function () {
 	var n = this.tail;
 	var html = "";
@@ -211,4 +331,111 @@ function CompiledPoetry () {
 	}
 	return html;
     }
+    
+    // Render over a canvas insetead of returning an HTML
+    // rendering
+    this.renderOnCanvas = function (zero) {
+	this.merge();
+
+	var margin = 6;
+	var font = 16;
+
+	if (zero) {
+	    html5.canvas.width = margin*2;
+	    html5.canvas.height = margin*2+font;
+	}
+
+	this.clearCanvas();
+
+	//console.log ("Target buffer is ("+html5.canvas.width+","+html5.canvas.height+")");
+	
+	var n = this.tail;
+	var p = [margin, margin];
+
+	html5.context.textBaseline = "top";
+	html5.context.textAlign = "left";
+	html5.context.font = font+"px monospace";
+	
+	while (n) {
+	    //console.log ("Filling with "+n.color+": "+n.data);
+
+	    var lines = this.textToLines(n.data);
+
+	    for (l in lines) {
+		html5.context.fillStyle = n.color;
+		html5.context.fillText (lines[l][0], p[0], p[1]);
+
+		p[0] += html5.context.measureText(lines[l][0]).width;
+
+		if (p[0] > html5.canvas.width) {
+		    html5.canvas.width = p[0]+margin;
+		    this.renderOnCanvas();
+		    return;
+		}
+
+		if (lines[l][1]) {
+		    p[0] = margin;
+		    p[1] += font;
+
+		    if (p[1]+font+margin > html5.canvas.height) {
+			html5.canvas.height = p[1]+font+margin;
+			this.renderOnCanvas();
+			return;
+		    }
+		}
+	    }
+
+	    n = n.next;
+	}
+
+	// Center the canvas
+	this.centerCanvas();
+    }
+
+    this.centerCanvas = function () {
+	// Get sizes
+	var w = $(window).width();
+	var h = $(window).height();
+	var cw = html5.canvas.width;
+	var ch = html5.canvas.height;
+
+	if (cw >= w/2)
+	    $("#poem").css("left", "0");
+	else
+	    $("#poem").css("left", ((w/2-cw)/2)+"px");
+	
+	if (ch >= h)
+	    $("#poem").css("top", "0");
+	else
+	    $("#poem").css("top", ((h-ch)/2)+"px");
+    }
+
+    this.textToLines = function (text) {
+	var lines = [];
+	lines.push(["", false]);
+
+	for (var c=0;c<text.length;c++) {
+	    if (text[c] == '\n') {
+		lines[lines.length-1][1] = true;
+		lines.push(["", false]);
+	    } else {
+		lines[lines.length-1][0] += text[c];
+	    }
+	}
+
+	return lines;
+    }
+
+    this.exportToWindow = function () {
+	window.open(html5.canvas.toDataURL("image/png"), "PEd Export", "width="+html5.canvas.width+",height="+html5.canvas.height);
+    }
+
+    this.clearCanvas = function () {
+	// html5.context.clearRect(0,0,html5.canvas.width,html5.canvas.height);
+	html5.context.fillStyle = "#272822";
+	html5.context.fillRect(0,0,html5.canvas.width,html5.canvas.height);
+    }
 }
+
+html5.getCanvas2dContext();
+html5.enableInput();
