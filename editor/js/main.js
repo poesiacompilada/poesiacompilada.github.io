20b2709830c00a61cafc48f0d1fb7c6a10c4dfc1
Felipe Tavares <felipe.oltavares@gmail.com>
2015-09-25 17:37:01 -0300

    Change URL, works better.

--- editor/js/main.js
+++ editor/js/main.js
@@ -9,7 +9,12 @@ var editor = CodeMirror(document.getElementById("editor"), {
 });
 
 function getUrl () {
-    return window.location.protocol+"//"+window.location.hostname+window.location.pathname.replace('index.html', 'view.html');
+    if (window.location.pathname.indexOf('index.html') > -1)
+	return window.location.protocol+"//"+window.location.hostname+window.location.pathname.replace('index.html', 'view.html');
+    else if (window.location.pathname.indexOf('?') > -1)
+	return window.location.protocol+"//"+window.location.hostname+window.location.pathname.replace('?', 'view.html?');
+    else
+	return window.location.protocol+"//"+window.location.hostname+window.location.pathname;
 }
 
 editor.addKeyMap(CodeMirror.normalizeKeyMap({
